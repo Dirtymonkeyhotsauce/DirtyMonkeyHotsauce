28c8766a79a9e1a8deb6d0aa4645417bec002a1a
Dirtymonkeyhotsauce <jaunw.marais@gmail.com>
2026-01-19 20:22:03 +0200

    Add click event to create spark elements

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("click", e => {
+  for (let i = 0; i < 12; i++) {
+    const spark = document.createElement("div");
+    spark.className = "spark";
+    spark.style.left = e.clientX + "px";
+    spark.style.top = e.clientY + "px";
+    spark.style.setProperty("--x", (Math.random() * 200 - 100) + "px");
+    spark.style.setProperty("--y", (Math.random() * -200) + "px");
+    document.body.appendChild(spark);
+
+    setTimeout(() => spark.remove(), 800);
+  }
+});
